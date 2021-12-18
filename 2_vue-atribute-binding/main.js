9c649b6d373b740a08780c44e8b791b602adee2c
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-12-19 01:19:26 +0700

    learn fundamental

---
+++ 2_vue-atribute-binding/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Shock",
+    image: "/image/vmSocks-green-onWhite.jpg",
+    altText: "A Pair of Sock"
+  }
+});
---
+++ 3_vue-conditional-rendering/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Shock",
+    image: "/image/vmSocks-green-onWhite.jpg",
+    altText: "A Pair of Sock",
+    inventory: 100
+  }
+});
